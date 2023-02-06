e63914e13aa67ef4a4e4218feaf054cfcd4b62e1
antonsadovskiy <antonsadovskiy6@gmail.com>
2023-02-06 16:42:24 +0300

    added OnOff components

--- src/App.tsx
+++ src/App.tsx
@@ -2,23 +2,22 @@ import React from 'react';
 import './App.css';
 import Accordion from "./components/Accordion/Accordion";
 import Rating from './components/Rating/Rating';
+import OnOff from "./components/OnOff/OnOff";
 
 function App() {
     return (
         <div>
-            <PageTitle title={"This is App component"}/>
-            <PageTitle title={"This isn't App component"}/>
-            Article 1
-            <Rating value={1}/>
+            <PageTitle title={"First title"}/>
             <Accordion titleValue={"Accordion 1"} collapsed={true}/>
-            <Accordion titleValue={"Accordion 2"} collapsed={false}/>
-            Article 2
-            <Rating value={5}/>
             <Rating value={4}/>
-            <Rating value={3}/>
+
+            <PageTitle title={"Second title"}/>
+            <Accordion titleValue={"Accordion 2"} collapsed={false}/>
             <Rating value={2}/>
-            <Rating value={1}/>
-            <Rating value={0}/>
+
+            <OnOff state={true}/>
+            <OnOff state={false}/>
+
         </div>
     );
 }
